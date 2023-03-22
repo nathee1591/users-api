73a6fbb7d0d95c6bc3a65f02488cec2da7a2156b
Natee Muenchananan <natee.tee159@gmail.com>
2023-03-22 14:35:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const mysql = require('mysql2')
+const app = express()
+
+app.use(cors())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/', (req, res) => {
+    console.log('Hello world')
+    res.send('Hello world!!')
+})
+
+app.get('/users', (req, res) => {
+    connection.query(
+        'SELECT * FROM users',
+        function(err, results, fields) {
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+app.listen(process.env.PORT || 3000)
