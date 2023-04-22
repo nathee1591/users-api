ba13b32526ad6fcf281db43586e9d2db9d4d2eb2
Natee Muenchananan <natee.tee159@gmail.com>
2023-04-22 17:42:05 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mysql = require('mysql2')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
@@ -17,6 +18,42 @@ app.get('/users', (req, res) => {
     connection.query(
         'SELECT * FROM users',
         function(err, results, fields) {
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+app.post('/create', (req, res) => {
+    connection.query(
+        'INSERT INTO users (fname, lname, username, password, avatar) VALUES (?, ?, ?, ?, ?)', [req.body.fname,req.body.lname,req.body.username, req.body.password, req.body.avatar],
+        function(err, results, fields) {
+            if (err) throw err;
+            console.log(results)
+            console.log(err)
+            res.send(results)
+        }
+    )
+})
+
+app.delete('/delete', (req, res) => {
+    connection.query(
+        'DELETE FROM users WHERE id = ?', [req.query.id],
+        function(err, results, fields) {
+            if (err) throw err;
+            console.log(results)
+            res.send(results)
+        }
+    )
+})
+
+app.put('/update', (req, res) => {
+    connection.query(
+        'UPDATE users SET id = ?, fname = ?, lname = ?, username = ?, password = ?, avatar = ? WHERE id = ?', 
+        [req.body.id, req.body.fname,
+        req.body.lname, req.body.username, req.body.password, req.body.avatar, req.body.id],
+        function(err, results, fields) {
+            if (err) throw err;
             console.log(results)
             res.send(results)
         }
